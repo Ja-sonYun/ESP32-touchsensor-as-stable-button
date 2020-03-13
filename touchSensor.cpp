ddd3e4f234df2b8a08ed17bb09b0f4f3987ae7cf
Ja-sonYun <46551097+Ja-sonYun@users.noreply.github.com>
2020-03-13 12:40:55 +0900

    Update touchSensor.cpp

--- touchSensor.cpp
+++ touchSensor.cpp
@@ -8,7 +8,7 @@ bool TouchSensor::isTouched() {
   if(_tRead > 0 /*for prevent vibrate*/ && _tRead < _readRange) {
     //prevent multiple input
     unsigned long currentTime = millis();
-    if(currentTime - previousTime <= 600) {
+    if(currentTime - previousTime <= 600) { // ignore touch until 600 ms if sensor detect touch
       return false;
     } else {
       previousTime = currentTime; 
