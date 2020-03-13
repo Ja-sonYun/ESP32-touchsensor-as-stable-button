f2eb526c6eec423e7b96871790732c4fc8a60064
Ja-sonYun <46551097+Ja-sonYun@users.noreply.github.com>
2020-03-13 12:36:10 +0900

    Update touchSensor.h

--- touchSensor.h
+++ touchSensor.h
@@ -7,13 +7,12 @@ class TouchSensor {
   private:
     unsigned long previousTime = 0; // prevent multiple touch
     int _tNum;  // the Pin number of touch sensor
-    int _tRead; // the Value from touch sensor
+    int _tRead = 0; // the Value from touch sensor
     int _readRange = 10; // the Range of the value, 0-60 or 0-150
     
   public:
-    TouchSensor(int tNum, int tRead) {
+    TouchSensor(int tNum) {
       _tNum = tNum;
-      _tRead = tRead;
     }
   public:
     void checkSensor();
